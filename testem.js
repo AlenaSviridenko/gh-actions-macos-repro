2a43e36f22479ff824a5b6e55659502698fb5369
Alyona Sviridenko <alenasviridenko@github.com>
2020-05-15 12:17:32 +0300

    restored mojave

--- testem.js
+++ testem.js
@@ -4,8 +4,7 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome',
-    'safari'
+    'Chrome'
   ],
   launch_in_dev: [
     'Chrome'
@@ -22,11 +21,6 @@ module.exports = {
         '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
-      ].filter(Boolean)
-    },
-    safari: {
-      ci: [
-        '--headless'
       ].filter(Boolean)
     }
   }
